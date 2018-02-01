387f833920bc458a6dc41e294c5618cbf846428c
Muthu <muthunarayananc@gmail.com>
2018-02-01 08:35:09 -0500

    changes to add esg for all focus

--- js/components.js
+++ js/components.js
@@ -65,7 +65,26 @@ AFRAME.registerComponent('get-esg-score', {
 
 		function RenderesgUsingD3(){
 
-			document.getElementById('esgtext').innerHTML='<p> The ESG Score is 90 </p>';
+			document.getElementById('esgtext').innerHTML='';
+      console.log("Get Data");
+			d3.json( "https://api.github.com/repos/mchellappa/coindemo/contents/esgscore.json", function(error,data){
+						if(error){
+							console.log(error);
+						} else {
+							console.log(data);
+						}
+						var decodedData = JSON.parse(window.atob(data.content));
+						console.log(decodedData.esgscore);
+						var svg = d3.select("#chartBars").append("svg:svg");
+						var viz1 = svg.append("rect")
+													.attr("width", decodedData.esgscore)
+														.attr("height",5)
+														.attr("fill","blue");
+
+
+						
+
+			});
 
 
 
@@ -73,6 +92,21 @@ AFRAME.registerComponent('get-esg-score', {
 		}
 
 	}
+});
+
+AFRAME.registerComponent('hide-esg', {
+	init() {
+        this.el.addEventListener('click', () => {
+
+			var chartPlaneWrapper = document.getElementById('esgscore');
+			chartPlaneWrapper.setAttribute('animation', 'property: position; startEvents: changeDisplay; to: -150 0 0; dur: 4000');
+			chartPlaneWrapper.emit('changeDisplay');
+
+			setTimeout(function(){
+				document.getElementById('esgscore').setAttribute('visible', false);
+			}, 4000);
+		 });
+    }
 });
 
 AFRAME.registerComponent('hide-graph', {
@@ -85,6 +119,210 @@ AFRAME.registerComponent('hide-graph', {
 
 			setTimeout(function(){
 				document.getElementById('chartPlaneWrapper').setAttribute('visible', false);
+			}, 4000);
+		 });
+    }
+});
+
+AFRAME.registerComponent('get-esg-score60', {
+	init() {
+
+		this.el.addEventListener('click', () => {
+
+			var cameraPosition = document.querySelector('[camera]').getAttribute('position'),
+			chartPlaneWrapper = document.getElementById('esgscore60'),
+			newPosition = {
+				x: cameraPosition.x,
+				y: cameraPosition.y,
+				z: cameraPosition.z - 3
+			};
+
+			Renderesg60UsingD3();
+
+			var animationDetails = 'property: position; startEvents: changeDisplay; to: ' + newPosition.x + ' ' + newPosition.y + ' ' + newPosition.z + ' dur: 3000';
+			esgscore.setAttribute('visible', 'true');
+			esgscore.setAttribute('animation',  animationDetails);
+			esgscore.emit('changeDisplay');
+
+		});
+
+		function Renderesg60UsingD3(){
+
+			document.getElementById('esgtext60').innerHTML='';
+      console.log("Get Data");
+			d3.json( "https://api.github.com/repos/mchellappa/coindemo/contents/esgscore60.json", function(error,data){
+						if(error){
+							console.log(error);
+						} else {
+							console.log(data);
+						}
+						var decodedData = JSON.parse(window.atob(data.content));
+						console.log(decodedData.esgscore);
+						var svg = d3.select("#chartBars").append("svg:svg");
+						var viz1 = svg.append("rect")
+													.attr("width", decodedData.esgscore)
+														.attr("height",5)
+														.attr("fill","blue");
+
+
+
+
+			});
+
+
+
+			document.getElementById('esgscore60').setAttribute('visible', true);
+		}
+
+	}
+});
+
+AFRAME.registerComponent('hide-esg60', {
+	init() {
+        this.el.addEventListener('click', () => {
+
+			var chartPlaneWrapper = document.getElementById('esgscore60');
+			chartPlaneWrapper.setAttribute('animation', 'property: position; startEvents: changeDisplay; to: -150 0 0; dur: 4000');
+			chartPlaneWrapper.emit('changeDisplay');
+
+			setTimeout(function(){
+				document.getElementById('esgscore60').setAttribute('visible', false);
+			}, 4000);
+		 });
+    }
+});
+
+AFRAME.registerComponent('get-esg-score70', {
+	init() {
+
+		this.el.addEventListener('click', () => {
+
+			var cameraPosition = document.querySelector('[camera]').getAttribute('position'),
+			chartPlaneWrapper = document.getElementById('esgscore70'),
+			newPosition = {
+				x: cameraPosition.x,
+				y: cameraPosition.y,
+				z: cameraPosition.z - 3
+			};
+
+			Renderesg70UsingD3();
+
+			var animationDetails = 'property: position; startEvents: changeDisplay; to: ' + newPosition.x + ' ' + newPosition.y + ' ' + newPosition.z + ' dur: 3000';
+			esgscore.setAttribute('visible', 'true');
+			esgscore.setAttribute('animation',  animationDetails);
+			esgscore.emit('changeDisplay');
+
+		});
+
+		function Renderesg70UsingD3(){
+
+			document.getElementById('esgtext70').innerHTML='';
+      console.log("Get Data");
+			d3.json( "https://api.github.com/repos/mchellappa/coindemo/contents/esgscore70.json", function(error,data){
+						if(error){
+							console.log(error);
+						} else {
+							console.log(data);
+						}
+						var decodedData = JSON.parse(window.atob(data.content));
+						console.log(decodedData.esgscore);
+						var svg = d3.select("#chartBars").append("svg:svg");
+						var viz1 = svg.append("rect")
+													.attr("width", decodedData.esgscore)
+														.attr("height",5)
+														.attr("fill","blue");
+
+
+
+
+			});
+
+
+
+			document.getElementById('esgscore70').setAttribute('visible', true);
+		}
+
+	}
+});
+
+AFRAME.registerComponent('hide-esg70', {
+	init() {
+        this.el.addEventListener('click', () => {
+
+			var chartPlaneWrapper = document.getElementById('esgscore70');
+			chartPlaneWrapper.setAttribute('animation', 'property: position; startEvents: changeDisplay; to: -150 0 0; dur: 4000');
+			chartPlaneWrapper.emit('changeDisplay');
+
+			setTimeout(function(){
+				document.getElementById('esgscore70').setAttribute('visible', false);
+			}, 4000);
+		 });
+    }
+});
+
+AFRAME.registerComponent('get-esg-score90', {
+	init() {
+
+		this.el.addEventListener('click', () => {
+
+			var cameraPosition = document.querySelector('[camera]').getAttribute('position'),
+			chartPlaneWrapper = document.getElementById('esgscore90'),
+			newPosition = {
+				x: cameraPosition.x,
+				y: cameraPosition.y,
+				z: cameraPosition.z - 3
+			};
+
+			Renderesg90UsingD3();
+
+			var animationDetails = 'property: position; startEvents: changeDisplay; to: ' + newPosition.x + ' ' + newPosition.y + ' ' + newPosition.z + ' dur: 3000';
+			esgscore.setAttribute('visible', 'true');
+			esgscore.setAttribute('animation',  animationDetails);
+			esgscore.emit('changeDisplay');
+
+		});
+
+		function Renderesg90UsingD3(){
+
+			document.getElementById('esgtext90').innerHTML='';
+      console.log("Get Data");
+			d3.json( "https://api.github.com/repos/mchellappa/coindemo/contents/esgscore90.json", function(error,data){
+						if(error){
+							console.log(error);
+						} else {
+							console.log(data);
+						}
+						var decodedData = JSON.parse(window.atob(data.content));
+						console.log(decodedData.esgscore);
+						var svg = d3.select("#chartBars").append("svg:svg");
+						var viz1 = svg.append("rect")
+													.attr("width", decodedData.esgscore)
+														.attr("height",5)
+														.attr("fill","blue");
+
+
+
+
+			});
+
+
+
+			document.getElementById('esgscore90').setAttribute('visible', true);
+		}
+
+	}
+});
+
+AFRAME.registerComponent('hide-esg90', {
+	init() {
+        this.el.addEventListener('click', () => {
+
+			var chartPlaneWrapper = document.getElementById('esgscore90');
+			chartPlaneWrapper.setAttribute('animation', 'property: position; startEvents: changeDisplay; to: -150 0 0; dur: 4000');
+			chartPlaneWrapper.emit('changeDisplay');
+
+			setTimeout(function(){
+				document.getElementById('esgscore90').setAttribute('visible', false);
 			}, 4000);
 		 });
     }
